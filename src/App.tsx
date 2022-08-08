44390b8999619ac1e1a75c53d15281cc650d831f
Pobarul-islam <pobarul3@gmail.com>
2022-08-08 10:02:53 +0600

    typing usestate added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
 import './App.css';
 
+type user = {
+  id: number,
+  name: string,
+  email: string
+
+}
 function App() {
-  const [user, setUsers] = useState<null>(null)
+  const [user, setUsers] = useState<user>({} as user)
   
   const handleAddUser = () => {
     setUsers({id: 1, name: "Pobarul isalm", email: "pobarulslam10@gmail.com"})
     console.log(user)
-  }
+  } 
   return (
     <div className="App">
       <h1>Users: </h1>
       <button  onClick={handleAddUser}>Add user</button>
+      <h2>{user.name}</h2>
      
+
      
     </div>
   );
